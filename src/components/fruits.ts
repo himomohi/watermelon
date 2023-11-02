c4e471cd6b03706c4f777c5e3d271a14914aa301
himomohi <Himomohi@gmail.com>
2023-11-02 14:44:59 +0900

    fruits.ts 에 각 과일별 점수 추가

--- src/components/fruits.ts
+++ src/components/fruits.ts
@@ -2,46 +2,57 @@ const FRUITS_BASE = [
     {
         name: 'base/00_cherry',
         radius: 33 / 2,
+        score: 1,
     },
     {
         name: 'base/01_strawberry',
         radius: 48 / 2,
+        score: 3,
     },
     {
         name: 'base/02_grape',
         radius: 61 / 2,
+        score: 5,
     },
     {
         name: 'base/03_gyool',
         radius: 69 / 2,
+        score: 7,
     },
     {
         name: 'base/04_orange',
         radius: 89 / 2,
+        score: 9,
     },
     {
         name: 'base/05_apple',
         radius: 114 / 2,
+        score: 11,
     },
     {
         name: 'base/06_pear',
         radius: 129 / 2,
+        score: 13,
     },
     {
         name: 'base/07_peach',
         radius: 156 / 2,
+        score: 15,
     },
     {
         name: 'base/08_pineapple',
         radius: 177 / 2,
+        score: 17,
     },
     {
         name: 'base/09_melon',
         radius: 220 / 2,
+        score: 19,
     },
     {
         name: 'base/10_watermelon',
         radius: 259 / 2,
+        score: 21,
     },
 ];
 
--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,179 +1,175 @@
-import React from "react";
-import { Bodies, Body, Engine, Events, Render, Runner, World } from "matter-js";
-import { FRUITS_BASE, FRUITS_HLW } from "./fruits";
-import "./dark.css";
+import React from 'react';
+import { Bodies, Body, Engine, Events, Render, Runner, World } from 'matter-js';
+import { FRUITS_BASE, FRUITS_HLW } from './fruits';
+import './dark.css';
 
 //실행라인
 const main = () => {
-  let THEME = "base"; // {base,halloween}
-  let FRUITS = FRUITS_BASE;
+    let THEME = 'base'; // {base,halloween}
+    let FRUITS = FRUITS_BASE;
 
-  //THEME 테마가 halloween 으로 입력되면 할로윈 버전으로
-  switch (THEME) {
-    case "halloween":
-      FRUITS = FRUITS_HLW;
-      break;
-    default:
-      FRUITS = FRUITS_BASE;
-  }
+    //THEME 테마가 halloween 으로 입력되면 할로윈 버전으로
+    switch (THEME) {
+        case 'halloween':
+            FRUITS = FRUITS_HLW;
+            break;
+        default:
+            FRUITS = FRUITS_BASE;
+    }
 
-  //엔진설정
-  const engine = Engine.create();
-  const render = Render.create({
-    engine,
-    element: document.body,
-    options: {
-      wireframes: false,
-      background: "#F7F4C8",
-      width: 620,
-      height: 850,
-    },
-  });
-  // 월드 그리기
-  const world = engine.world;
+    //엔진설정
+    const engine = Engine.create();
+    const render = Render.create({
+        engine,
+        element: document.body,
+        options: {
+            wireframes: false,
+            background: '#F7F4C8',
+            width: 620,
+            height: 850,
+        },
+    });
+    // 월드 그리기
+    const world = engine.world;
 
-  //벽설정
-  const leftWall = Bodies.rectangle(15, 395, 30, 790, {
-    isStatic: true,
-    render: { fillStyle: "#0d0c09" },
-  });
-
-  const rightWall = Bodies.rectangle(605, 395, 30, 790, {
-    isStatic: true,
-    render: { fillStyle: "#0d0c09" },
-  });
-
-  const ground = Bodies.rectangle(310, 820, 620, 60, {
-    isStatic: true,
-    render: { fillStyle: "#0d0c09" },
-  });
-  const topLine = Bodies.rectangle(310, 150, 620, 2, {
-    isStatic: true,
-    isSensor: true,
-    render: { fillStyle: "#0d0c09" },
-  });
-
-  World.add(world, [leftWall, rightWall, ground, topLine]);
-
-  Render.run(render); // 렌더
-  Runner.run(engine); //런너
-
-  //값 초기화
-  let currentBody = null;
-  let currentFruit = null;
-  let disableAction = false;
-  let interval = null;
-
-  //과일추가
-  function addFruit() {
-    const index = Math.floor(Math.random() * 5);
-    const fruit = FRUITS[index];
-
-    const body = Bodies.circle(300, 50, fruit.radius, {
-      index: index,
-      isSleeping: true,
-      render: {
-        sprite: { texture: `${fruit.name}.png` },
-      },
-      restitution: 0.2,
+    //벽설정
+    const leftWall = Bodies.rectangle(15, 395, 30, 790, {
+        isStatic: true,
+        render: { fillStyle: '#0d0c09' },
     });
 
-    currentBody = body;
-    currentFruit = fruit;
-    World.add(world, body);
-  }
-  //키입력
+    const rightWall = Bodies.rectangle(605, 395, 30, 790, {
+        isStatic: true,
+        render: { fillStyle: '#0d0c09' },
+    });
 
-  window.onkeydown = (event) => {
-    if (disableAction) {
-      return;
+    const ground = Bodies.rectangle(310, 820, 620, 60, {
+        isStatic: true,
+        render: { fillStyle: '#0d0c09' },
+    });
+    const topLine = Bodies.rectangle(310, 150, 620, 2, {
+        isStatic: true,
+        isSensor: true,
+        render: { fillStyle: '#0d0c09' },
+    });
+
+    World.add(world, [leftWall, rightWall, ground, topLine]);
+
+    Render.run(render); // 렌더
+    Runner.run(engine); //런너
+
+    //값 초기화
+    let currentBody = null;
+    let currentFruit = null;
+    let disableAction = false;
+    let interval = null;
+
+    //과일추가
+    function addFruit() {
+        const index = Math.floor(Math.random() * 5);
+        const fruit = FRUITS[index];
+
+        const body = Bodies.circle(300, 50, fruit.radius, {
+            index: index,
+            isSleeping: true,
+            render: {
+                sprite: { texture: `${fruit.name}.png` },
+            },
+            restitution: 0.2,
+        });
+
+        currentBody = body;
+        currentFruit = fruit;
+        World.add(world, body);
     }
+    //키입력
 
-    switch (event.code) {
-      case "KeyA":
-        if (interval) return;
-        interval = setInterval(() => {
-          if (currentBody.position.x - currentFruit.radius > 30)
-            Body.setPosition(currentBody, {
-              x: currentBody.position.x - 1,
-              y: currentBody.position.y,
-            });
-        }, 5);
-        break;
-
-      case "KeyD":
-        if (interval) return;
-        interval = setInterval(() => {
-          if (currentBody.position.x + currentFruit.radius < 590)
-            Body.setPosition(currentBody, {
-              x: currentBody.position.x + 1,
-              y: currentBody.position.y,
-            });
-        }, 5);
-        break;
-
-      case "KeyS":
-        currentBody.isSleeping = false;
-        disableAction = true;
-
-        setTimeout(() => {
-          addFruit();
-          disableAction = false;
-        }, 1000);
-
-        break;
-    }
-  };
-
-  //키를 땔때
-  window.onkeyup = (event) => {
-    switch (event.code) {
-      case "KeyA":
-      case "KeyD":
-        clearInterval(interval);
-        interval = null;
-    }
-  };
-  Events.on(engine, "collisionStart", (event) => {
-    event.pairs.forEach((collision) => {
-      if (collision.bodyA.index === collision.bodyB.index) {
-        const index = collision.bodyA.index;
-
-        if (index === FRUITS.length - 1) {
-          return;
+    window.onkeydown = (event) => {
+        if (disableAction) {
+            return;
         }
 
-        World.remove(world, [collision.bodyA, collision.bodyB]);
+        switch (event.code) {
+            case 'KeyA':
+                if (interval) return;
+                interval = setInterval(() => {
+                    if (currentBody.position.x - currentFruit.radius > 30)
+                        Body.setPosition(currentBody, {
+                            x: currentBody.position.x - 1,
+                            y: currentBody.position.y,
+                        });
+                }, 5);
+                break;
 
-        const newFruit = FRUITS[index + 1];
+            case 'KeyD':
+                if (interval) return;
+                interval = setInterval(() => {
+                    if (currentBody.position.x + currentFruit.radius < 590)
+                        Body.setPosition(currentBody, {
+                            x: currentBody.position.x + 1,
+                            y: currentBody.position.y,
+                        });
+                }, 5);
+                break;
 
-        const newBody = Bodies.circle(
-          collision.collision.supports[0].x,
-          collision.collision.supports[0].y,
-          newFruit.radius,
-          {
-            render: {
-              sprite: { texture: `${newFruit.name}.png` },
-            },
-            index: index + 1,
-          }
-        );
+            case 'KeyS':
+                currentBody.isSleeping = false;
+                disableAction = true;
 
-        World.add(world, newBody);
-      }
+                setTimeout(() => {
+                    addFruit();
+                    disableAction = false;
+                }, 1000);
 
-      if (
-        !disableAction &&
-        (collision.bodyA.name === "topLine" ||
-          collision.bodyB.name === "topLine")
-      ) {
-        alert("Game over");
-      }
+                break;
+        }
+    };
+
+    //키를 땔때
+    window.onkeyup = (event) => {
+        switch (event.code) {
+            case 'KeyA':
+            case 'KeyD':
+                clearInterval(interval);
+                interval = null;
+        }
+    };
+    Events.on(engine, 'collisionStart', (event) => {
+        event.pairs.forEach((collision) => {
+            if (collision.bodyA.index === collision.bodyB.index) {
+                const index = collision.bodyA.index;
+
+                if (index === FRUITS.length - 1) {
+                    return;
+                }
+
+                World.remove(world, [collision.bodyA, collision.bodyB]);
+
+                const newFruit = FRUITS[index + 1];
+
+                const newBody = Bodies.circle(
+                    collision.collision.supports[0].x,
+                    collision.collision.supports[0].y,
+                    newFruit.radius,
+                    {
+                        render: {
+                            sprite: { texture: `${newFruit.name}.png` },
+                        },
+                        index: index + 1,
+                    }
+                );
+
+                World.add(world, newBody);
+            }
+
+            if (!disableAction && (collision.bodyA.name === 'topLine' || collision.bodyB.name === 'topLine')) {
+                alert('Game over');
+            }
+        });
     });
-  });
-  addFruit();
+    addFruit();
 
-  //   return <div></div>;
+    //   return <div></div>;
 };
 
 export default main;
