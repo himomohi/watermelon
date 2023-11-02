9b738adbfb90155414cc172dffc705ac86926d2a
himomohi <himomohi@gmail.com>
2023-11-02 13:34:29 +0900

    엄격모드 제한중

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import Main from '../src/components/main';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Main from "../src/components/main";
 // import Container from '../src/components/Container';
 
 function App() {
-    return (
-        <>
-            <Main />
-        </>
-    );
+  return (
+    <>
+      <Main />
+    </>
+  );
 }
 
 export default App;
